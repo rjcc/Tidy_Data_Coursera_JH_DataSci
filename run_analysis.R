dd71a2ff3ce5808959f652fea074b6eb831306c9
Robert Chen <rjcc@me.com>
2019-06-09 20:14:45 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 setwd("/Users/RJCC/Dropbox/@Next/AI/JH_data_prep/UCI HAR Dataset")
 library(dplyr)
 
-# This script,run_analysis.R, does the following.
+# This R script "run_analysis.R" does the following:
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set
